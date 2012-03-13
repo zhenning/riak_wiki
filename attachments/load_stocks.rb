0f4e483cf1c2f01c958f4df285c11c431c0f62be
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-03-12 22:43:37 -0300

    Use correct port number.
    As the fast track instructions configure riak to run on 8091-3, update the stock uploader to use the correct port number for loading data.

--- attachments/load_stocks.rb
+++ attachments/load_stocks.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 %w[rubygems csv time riak].each{|lib| require lib}
 
-client  = Riak::Client.new
+client  = Riak::Client.new(:port => 8091)
 bucket  = client['goog']
 
 quotes  = CSV.read 'goog.csv'
